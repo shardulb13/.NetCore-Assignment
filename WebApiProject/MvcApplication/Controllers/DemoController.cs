a67e5b726ea3a752ab12eb4fb75745c66729c314
shardulb13 <sharbhingardive777@gmail.com>
2022-08-09 12:00:36 +0530

    Added Controller Assignment

---
+++ WebApiProject/MvcApplication/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcApplication.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApiProject/WebApiProject/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiProject.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class StudentController
+    {
+
+        [HttpGet]
+        public IEnumerable<Student> GetStudent()
+        {
+            return StudentDetails();
+        }
+       
+        [HttpPost]
+        public string PostStudent()
+        {
+            return "This is post method";
+        }
+
+        [HttpPut]
+        public string PutStudents()
+        {
+            return "This is put Method";
+        }
+
+       
+        private List<Student> StudentDetails()
+        {
+            return new List<Student>()
+            {
+                new Student()
+                {
+                    id = 1,
+                    name = "Shardul"
+                }
+            };
+        }
+    }
+}
